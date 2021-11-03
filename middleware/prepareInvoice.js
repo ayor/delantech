f1fd8ea006904ca4a37367c87689aec1382321d8
ayor <dosumuayomide@gmail.com>
2021-11-03 14:11:24 +0100

    updating invoices to s3

--- middleware/prepareInvoice.js
+++ middleware/prepareInvoice.js
@@ -1,70 +1,65 @@
 const PDFDOC = require('easyinvoice');
-const fs = require('fs');
-const path = require('path');
 
-
-const createInvoice = async (order, customer ) => {
-
-    try {
-
-        if (!order) {
-            const err = new Error('there are no orders')
-            err.statusCode = 404;
-            throw err;
-        }
-
-        const invoiceName = 'invoice_' + customer.email + '_' + order.id + '.pdf';
-
-        const products = order.items.map(item => {
-            return {
-                "quantity": item.qty.toString(),
-                "description": item.name,
-                "price": parseFloat(item.price),
-                "tax": 0
-            }
-        })
-
-        let data = {
-            //"documentTitle": "RECEIPT", //Defaults to INVOICE
-            "currency": "NGN",
-            "taxNotation": "vat", //or gst
-            "marginTop": 25,
-            "marginRight": 25,
-            "marginLeft": 25,
-            "marginBottom": 25,
-            "logo": "https://delantech.com.ng/images/Delantech.png", //or base64
-            //"logoExtension": "png", //only when logo is base64
-            "sender": {
-                "company": "Delantech",
-                "address": "TGS Plaza, Bogije Road, Lekki Express Way, Lekki Lagos.",
-                "zip": "100264",
-                "city": "Lagos",
-                "country": "Nigeria"
-                //"custom1": "custom value 1",
-                //"custom2": "custom value 2",
-                //"custom3": "custom value 3"
-            },
-            "client": {
-                "company": customer.email,
-                "address": customer.address,
-                "zip": "",
-                "city": customer.city,
-                "country": customer.country
-                //"custom1": "custom value 1",
-                //"custom2": "custom value 2",
-                //"custom3": "custom value 3"
-            },
-            "invoiceNumber": order.id,
-            "invoiceDate": new Date().toLocaleDateString(),
-            "products": products,
-            // "bottomNotice": "This is invoice excludes a " + invoiceOrder.shippingMode + " shipping fee of $" + invoiceOrder.shippingFee
-        };
-        const result = await PDFDOC.createInvoice(data);
-         fs.writeFileSync(path.join('invoices', invoiceName), result.pdf, 'base64');
-        return { invoiceName, path: path.join( 'invoices', invoiceName) }
-    } catch (error) {
-        throw error;
+const createInvoice = async (order, customer) => {
+  try {
+    if (!order) {
+      const err = new Error('there are no orders');
+      err.statusCode = 404;
+      throw err;
     }
-}
+
+    const invoiceName = 'invoice_' + customer.email + '_' + order.id + '.pdf';
+
+    const products = order.items.map((item) => {
+      return {
+        quantity: item.qty.toString(),
+        description: item.name,
+        price: parseFloat(item.price),
+        tax: 0,
+      };
+    });
+
+    let data = {
+      //"documentTitle": "RECEIPT", //Defaults to INVOICE
+      currency: 'NGN',
+      taxNotation: 'vat', //or gst
+      marginTop: 25,
+      marginRight: 25,
+      marginLeft: 25,
+      marginBottom: 25,
+      logo: 'https://delantech.vercel.app/img/sfav.png', //or base64
+      //"logoExtension": "png", //only when logo is base64
+      sender: {
+        company: 'Delantech',
+        address: 'TGS Plaza, Bogije Road, Lekki Express Way, Lekki Lagos.',
+        zip: '100264',
+        city: 'Lagos',
+        country: 'Nigeria',
+        //"custom1": "custom value 1",
+        //"custom2": "custom value 2",
+        //"custom3": "custom value 3"
+      },
+      client: {
+        company: customer.email,
+        address: customer.address,
+        zip: '',
+        city: customer.city,
+        country: customer.country,
+        //"custom1": "custom value 1",
+        //"custom2": "custom value 2",
+        //"custom3": "custom value 3"
+      },
+      invoiceNumber: order.id,
+      invoiceDate: new Date().toLocaleDateString(),
+      products: products,
+      // "bottomNotice": "This is invoice excludes a " + invoiceOrder.shippingMode + " shipping fee of $" + invoiceOrder.shippingFee
+    };
+    const result = await PDFDOC.createInvoice(data);
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = createInvoice;
--- pages/api/order.js
+++ pages/api/order.js
@@ -2,6 +2,17 @@ const nodemailer = require('nodemailer');
 const { validateEmail } = require('../../middleware/validate');
 import { db } from '../../middleware/db';
 const createInvoice = require('../../middleware/prepareInvoice');
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+const S3 = new AWS.S3();
+
+AWS.config.getCredentials(function (err) {
+  if (err) console.log(err.stack);
+  // credentials not loaded
+  else {
+    console.log('Access key:', AWS.config.credentials.accessKeyId);
+  }
+});
 
 const _usermail = 'lawaltoyeeb@delantech.com.ng';
 const _userpass = process.env.PASS;
@@ -38,37 +49,67 @@ export default async function handler(req, res) {
     });
 
     if (dbResult.id) {
-      //   invoice =  await createInvoice({ ...order, id: dbResult.id }, { email, phone, address, city, country });
-      //     console.log(invoice)
-      let info = await transporter.sendMail({
-        from: email, // sender address
-        to: 'info@delantech.com.ng', // list of receivers
-        subject: `Order from 
-             ${email}`, // Subject line
-        text: ` Hello Delantech, 
-            Kindly find attached order request from ${email}.
-            
-            You can call - ${phone} for more detials
-            `, // plain text body
-        // attachments: [{
-        //     filename: invoice.invoiceName,
-        //     path: invoice.path,
-        //     contentType: 'application/pdf'
-        // }],
-      });
-      let reply = await transporter.sendMail({
-        to: email, // list of receivers
-        subject: `Reply ${info.messageId}`, // Subject line
-        text: ` Hello ${email}, 
-            Kindly find attached your order invoice.
-            
-            A personnel from Delantech will get in touch with you as soon as possible.
-            `, // plain text body
-        // attachments: [{
-        //     filename: invoice.invoiceName,
-        //     path: invoice.path,
-        //     contentType: 'application/pdf'
-        // }],
+      invoice = await createInvoice(
+        { ...order, id: dbResult.id },
+        { email, phone, address, city, country }
+      );
+      const s3_data = {
+        key: dbResult.id,
+        bucket: 'delantech-invoices',
+        contentType: 'application/pdf',
+        body: invoice.pdf,
+        contentEncoding: 'base64',
+      };
+
+      /**
+         * const bucket = new AWS.S3(); 
+         fs.writeFileSync(path.join('invoices', invoiceName), result.pdf, 'base64');
+        return { invoiceName, path: path.join( 'invoices', invoiceName) }
+         */
+
+      S3.putObject(s3_data, async (err, resp) => {
+        if (err) {
+          throw new Error('Could not upload invoice to aws s3 bucket');
+        }
+
+        console.log(resp);
+
+        const object = S3.getObject({
+          Bucket: 'delantech-invoice',
+          Key: dbResult.id,
+        });
+
+        console.log(object);
+        // let info = await transporter.sendMail({
+        //   from: email, // sender address
+        //   to: 'info@delantech.com.ng', // list of receivers
+        //   subject: `Order from
+        //        ${email}`, // Subject line
+        //   text: ` Hello Delantech,
+        //       Kindly find attached order request from ${email}.
+
+        //       You can call - ${phone} for more detials
+        //       `, // plain text body
+        //   // attachments: [{
+        //   //     filename: invoice.invoiceName,
+        //   //     path: invoice.path,
+        //   //     contentType: 'application/pdf'
+        //   // }],
+        // });
+        // let reply = await transporter.sendMail({
+        //   to: email, // list of receivers
+        //   subject: `Reply ${info.messageId}`, // Subject line
+        //   text: ` Hello ${email},
+        //       Kindly find attached your order invoice.
+
+        //       A personnel from Delantech will get in touch with you as soon as possible.
+        //       `, // plain text body
+        //   // attachments: [{
+        //   //     filename: invoice.invoiceName,
+        //   //     path: invoice.path,
+        //   //     contentType: 'application/pdf'
+        //   // }],
+        // });
       });
     }
     res.status(200).json({ message: 'Order Received' });
